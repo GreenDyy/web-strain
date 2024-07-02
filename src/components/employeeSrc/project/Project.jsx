12b3b0322a87f82e2298b7974c1e1bda78fbabc8
Huỳnh Khánh Duy <103752303+KhanhDuyyyy@users.noreply.github.com>
2024-07-02 12:25:51 +0700

    final that nè =))

--- src/components/employeeSrc/project/Project.jsx
+++ src/components/employeeSrc/project/Project.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import './Project.scss'
-import { getAllProjectEmployeeApi } from "../../../apis/apiTask";
+import { getAllEmployeeByIdProjectApi, getAllProjectEmployeeApi } from "../../../apis/apiTask";
 import { getEmployeeByIdApi } from "../../../apis/apiLoginEmployee";
 import { icons } from "../../../constants";
 import { formatDate } from "../../../utils/Utils";
+import DetailProject from "./DetailProject";
 
-const ItemProject = ({ item }) => {
+const ItemProject = ({ item, handleShowDetail }) => {
     const [owner, setOwner] = useState(null)
     useEffect(() => {
         const fetchData = async () => {
@@ -18,11 +19,11 @@ const ItemProject = ({ item }) => {
         <div className="item-project">
             <div className="row-1">
                 <div className="col-1">
-                    <img src={icons.project} className="icon-project" />
+                    <img src={icons.project} className="icon-project" alt="project icon" />
                     <h3 className="name-project">{item?.projectName}</h3>
                 </div>
 
-                <img src={icons.more} className="icon" />
+                <img src={icons.more} className="icon" alt="more icon" onClick={() => handleShowDetail(item.idProject)} />
             </div>
             <p className="row-2">Tạo bởi {owner?.fullName} vào ngày {formatDate(item.startDateProject)}</p>
             <p className={`${item?.status === 'Chưa hoàn thành' ? 'status-notcomplete' : 'status-complete'}`}>{item?.status}</p>
@@ -32,24 +33,37 @@ const ItemProject = ({ item }) => {
 
 function Project({ employee }) {
     const [projects, setProjects] = useState([])
+    const [dataModal, setDataModal] = useState(null)
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         const fetchData = async () => {
-            const projects = await getAllProjectEmployeeApi(employee?.idEmployee)
-            setProjects(projects.data)
+            const projectsData = await getAllProjectEmployeeApi(employee?.idEmployee)
+            setProjects(projectsData.data)
         }
         fetchData()
-    }, [])
+    }, [employee?.idEmployee])
+
+    const handleShowDetail = async (idProject) => {
+        const response = await getAllEmployeeByIdProjectApi(idProject)
+        setDataModal(response.data)
+        setShowModal(true)
+    }
+
     console.log(projects)
+
     return (
         <div className="Project">
             <p className="text-header">Các dự án tham gia</p>
             <div className="wrap-all-item">
                 {projects?.map((item, index) => (
-                    <ItemProject key={index} item={item} />
+                      <ItemProject key={index} item={item} handleShowDetail={handleShowDetail} handleCloseModal={() => { setShowModal(false) }} />
                 ))}
 
             </div>
+            {showModal && (
+                <DetailProject item={dataModal} handleCloseModal={() => setShowModal(false)} />
+            )}
         </div>
     )
 }
